31e3b8e9f9426ea4215c19cfcef0ef00de2aef3e
KeivnRangel <rangel.moreira@outlook.com>
2016-05-27 14:25:22 -0300

    Classe de entidde categoria atualizada

---
+++ SistemaTCC/Projeto/php/Dao/VendaDao.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kevin_000
+ * Date: 27/05/2016
+ * Time: 00:19
+ */
+class CaixaDao
+{
+        
+    
+}
---
+++ SistemaTCC/Projeto/php/Ent/Venda.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kevin_000
+ * Date: 27/05/2016
+ * Time: 00:20
+ */
+class Caixa
+{
+    public $totalParcelas;
+//    public 
+//    public $
+
+}
