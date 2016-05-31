d338f5847df4c426c030827846c3dae1f97ebb1f
KeivnRangel <rangel.moreira@outlook.com>
2016-05-31 00:44:32 -0300

    atualização da classe ItemVenda  31/05

--- SistemaTCC/Projeto/php/Ent/itemVenda.php
+++ SistemaTCC/Projeto/php/Ent/itemVenda.php
@@ -7,13 +7,99 @@
  * Time: 17:44
  */
 
-$produto =new Produto();
+
 class itemVenda
 {
-
+        public $idItemVenda;
+        public  $idVenda;
+        public $idProduto;
         public  $qtd;
         public $subtotal;
+        public $dataHoraCadastro;
+        public $dataHoraAtualizacao;
 
+    /**
+     * @return mixed
+     */
+    public function getIdItemVenda()
+    {
+        return $this->idItemVenda;
+    }
+
+    /**
+     * @param mixed $idItemVenda
+     */
+    public function setIdItemVenda($idItemVenda)
+    {
+        $this->idItemVenda = $idItemVenda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataHoraCadastro()
+    {
+        return $this->dataHoraCadastro;
+    }
+
+    /**
+     * @param mixed $dataHoraCadastro
+     */
+    public function setDataHoraCadastro($dataHoraCadastro)
+    {
+        $this->dataHoraCadastro = $dataHoraCadastro;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdVenda()
+    {
+        return $this->idVenda;
+    }
+
+    /**
+     * @param mixed $idVenda
+     */
+    public function setIdVenda($idVenda)
+    {
+        $this->idVenda = $idVenda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdProduto()
+    {
+        return $this->idProduto;
+    }
+
+    /**
+     * @param mixed $idProduto
+     */
+    public function setIdProduto($idProduto)
+    {
+        $this->idProduto = $idProduto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataHoraAtualizacao()
+    {
+        return $this->dataHoraAtualizacao;
+    }
+
+    /**
+     * @param mixed $dataHoraAtualizacao
+     */
+    public function setDataHoraAtualizacao($dataHoraAtualizacao)
+    {
+        $this->dataHoraAtualizacao = $dataHoraAtualizacao;
+    }
+    
+    
+    
     /**
      * @return mixed
      */
